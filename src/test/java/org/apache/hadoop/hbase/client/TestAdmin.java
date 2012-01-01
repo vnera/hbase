5e75b4cb3fa73b35d6802e43b4eda8cbc2fbb34c
Zhihong Yu <tedyu@apache.org>
2012-01-01 17:46:53 +0000

    HBASE-5114 TestMetaReaderEditor can timeout during test execution
    Reason: Test
    Author: nkeywal
    Ref: CDH_7349

--- src/test/java/org/apache/hadoop/hbase/client/TestAdmin.java
+++ src/test/java/org/apache/hadoop/hbase/client/TestAdmin.java
@@ -42,10 +42,8 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HServerAddress;
-import org.apache.hadoop.hbase.HServerInfo;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.NotServingRegionException;
-import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableExistsException;
 import org.apache.hadoop.hbase.TableNotDisabledException;
 import org.apache.hadoop.hbase.TableNotEnabledException;
@@ -53,17 +51,12 @@ import org.apache.hadoop.hbase.TableNotFoundException;
 import org.apache.hadoop.hbase.executor.EventHandler;
 import org.apache.hadoop.hbase.executor.EventHandler.EventType;
 import org.apache.hadoop.hbase.executor.ExecutorService;
-import org.apache.hadoop.hbase.ipc.HRegionInterface;
 import org.apache.hadoop.hbase.master.MasterServices;
 import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
-import org.apache.hadoop.hbase.regionserver.wal.HLog;
 import org.apache.hadoop.hbase.regionserver.wal.TestHLogUtils;
 import org.apache.hadoop.hbase.InvalidFamilyOperationException;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.hbase.zookeeper.ZKAssign;
-import org.apache.hadoop.hbase.zookeeper.ZooKeeperWatcher;
-import org.apache.zookeeper.KeeperException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -1293,36 +1286,6 @@ public class TestAdmin {
 
     admin.createTable(htd, null);
   }
-
-
-  @Test
-  public void testHundredsOfTable() throws IOException{
-    final int times = 100;
-    HColumnDescriptor fam1 = new HColumnDescriptor("fam1");
-    HColumnDescriptor fam2 = new HColumnDescriptor("fam2");
-    HColumnDescriptor fam3 = new HColumnDescriptor("fam3");
-
-    for(int i = 0; i < times; i++) {
-      HTableDescriptor htd = new HTableDescriptor("table"+i);
-      htd.addFamily(fam1);
-      htd.addFamily(fam2);
-      htd.addFamily(fam3);
-      this.admin.createTable(htd);
-    }
-
-    for(int i = 0; i < times; i++) {
-      String tableName = "table"+i;
-      this.admin.disableTable(tableName);
-      byte [] tableNameBytes = Bytes.toBytes(tableName);
-      assertTrue(this.admin.isTableDisabled(tableNameBytes));
-      this.admin.enableTable(tableName);
-      assertFalse(this.admin.isTableDisabled(tableNameBytes));
-      this.admin.disableTable(tableName);
-      assertTrue(this.admin.isTableDisabled(tableNameBytes));
-      this.admin.deleteTable(tableName);
-    }
-  }
-  
 
   /**
    * For HBASE-2556
