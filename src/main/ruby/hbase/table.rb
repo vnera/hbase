efde04e0f0a4f29fe1fcd63a68b4267cfd10e38d
David S. Wang <dsw@cloudera.com>
2012-05-13 06:14:25 -0700

    HBASE-5973: Add ability for potentially long-running IPC calls to abort if client disconnects
    Reason: Improvement
    Author: Todd Lipcon
    Ref: CDH-5769

--- src/main/ruby/hbase/table.rb
+++ src/main/ruby/hbase/table.rb
@@ -219,7 +219,8 @@ module Hbase
         stoprow = args["STOPROW"]
         timestamp = args["TIMESTAMP"]
         columns = args["COLUMNS"] || args["COLUMN"] || get_all_columns
-        cache = args["CACHE_BLOCKS"] || true
+        cache_blocks = args["CACHE_BLOCKS"] || true
+        cache = args["CACHE"] || 0
         versions = args["VERSIONS"] || 1
         timerange = args[TIMERANGE]
 
@@ -251,7 +252,8 @@ module Hbase
         end
 
         scan.setTimeStamp(timestamp) if timestamp
-        scan.setCacheBlocks(cache)
+        scan.setCacheBlocks(cache_blocks)
+        scan.setCaching(cache) if cache > 0
         scan.setMaxVersions(versions) if versions > 1
         scan.setTimeRange(timerange[0], timerange[1]) if timerange
       else
