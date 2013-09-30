c27a70309d3cd8b63ceedecac17d4fec2b88277d
Matteo Bertozzi <matteo.bertozzi@cloudera.com>
2013-09-30 23:18:04 +0100

    HBASE-9663 PerformanceEvaluation does not properly honor specified table name parameter
    Reason: Bug
    Author: Matteo Bertozzi
    Ref: CDH-14536
    
    Conflicts:
    
    	src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java

--- src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
+++ src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
@@ -140,7 +140,8 @@ public class PerformanceEvaluation extends Configured implements Tool {
 
   /** Regex to parse lines in input file passed to mapreduce task. */
   public static final Pattern LINE_PATTERN =
-    Pattern.compile("startRow=(\\d+),\\s+" +
+    Pattern.compile("tableName=(\\w+),\\s+" +
+        "startRow=(\\d+),\\s+" +
         "perClientRunRows=(\\d+),\\s+" +
         "totalRows=(\\d+),\\s+" +
         "sampleRate=([-+]?[0-9]*\\.?[0-9]+),\\s+" +
@@ -217,6 +218,7 @@ public class PerformanceEvaluation extends Configured implements Tool {
    *  the record value is the PeInputSplit itself.
    */
   public static class PeInputSplit extends InputSplit implements Writable {
+    private byte[] tableName = TABLE_NAME;
     private int startRow = 0;
     private int rows = 0;
     private int totalRows = 0;
@@ -228,8 +230,10 @@ public class PerformanceEvaluation extends Configured implements Tool {
 
     public PeInputSplit() {}
 
-    public PeInputSplit(int startRow, int rows, int totalRows, float sampleRate,
-        int clients, boolean flushCommits, boolean writeToWAL, boolean reportLatency) {
+    public PeInputSplit(byte[] tableName, int startRow, int rows, int totalRows, 
+        float sampleRate, int clients, boolean flushCommits, boolean writeToWAL,
+        boolean reportLatency) {
+      this.tableName = tableName;
       this.startRow = startRow;
       this.rows = rows;
       this.totalRows = totalRows;
@@ -242,6 +246,10 @@ public class PerformanceEvaluation extends Configured implements Tool {
 
     @Override
     public void readFields(DataInput in) throws IOException {
+      int tableNameLen = in.readInt();
+      this.tableName = new byte[tableNameLen];
+      in.readFully(this.tableName);
+
       this.startRow = in.readInt();
       this.rows = in.readInt();
       this.totalRows = in.readInt();
@@ -254,6 +262,8 @@ public class PerformanceEvaluation extends Configured implements Tool {
 
     @Override
     public void write(DataOutput out) throws IOException {
+      out.writeInt(tableName.length);
+      out.write(tableName);
       out.writeInt(startRow);
       out.writeInt(rows);
       out.writeInt(totalRows);
@@ -272,6 +282,10 @@ public class PerformanceEvaluation extends Configured implements Tool {
     @Override
     public String[] getLocations() throws IOException, InterruptedException {
       return new String[0];
+    }
+
+    public byte[] getTableName() {
+      return tableName;
     }
 
     public int getStartRow() {
@@ -332,27 +346,29 @@ public class PerformanceEvaluation extends Configured implements Tool {
           }
           Matcher m = LINE_PATTERN.matcher(lineText.toString());
           if((m != null) && m.matches()) {
-            int startRow = Integer.parseInt(m.group(1));
-            int rows = Integer.parseInt(m.group(2));
-            int totalRows = Integer.parseInt(m.group(3));
-            float sampleRate = Float.parseFloat(m.group(4));
-            int clients = Integer.parseInt(m.group(5));
-            boolean flushCommits = Boolean.parseBoolean(m.group(6));
-            boolean writeToWAL = Boolean.parseBoolean(m.group(7));
-            boolean reportLatency = Boolean.parseBoolean(m.group(8));
+            byte[] tableName = Bytes.toBytes(m.group(1));
+            int startRow = Integer.parseInt(m.group(2));
+            int rows = Integer.parseInt(m.group(3));
+            int totalRows = Integer.parseInt(m.group(4));
+            float sampleRate = Float.parseFloat(m.group(5));
+            int clients = Integer.parseInt(m.group(6));
+            boolean flushCommits = Boolean.parseBoolean(m.group(7));
+            boolean writeToWAL = Boolean.parseBoolean(m.group(8));
+            boolean reportLatency = Boolean.parseBoolean(m.group(9));
 
-            LOG.debug("split["+ splitList.size() + "] " +
-                     " startRow=" + startRow +
-                     " rows=" + rows +
-                     " totalRows=" + totalRows +
-                     " sampleRate=" + sampleRate +
-                     " clients=" + clients +
-                     " flushCommits=" + flushCommits +
-                     " writeToWAL=" + writeToWAL +
-                     " reportLatency=" + reportLatency);
+            LOG.debug("tableName=" + tableName +
+                      " split["+ splitList.size() + "] " +
+                      " startRow=" + startRow +
+                      " rows=" + rows +
+                      " totalRows=" + totalRows +
+                      " sampleRate=" + sampleRate +
+                      " clients=" + clients +
+                      " flushCommits=" + flushCommits +
+                      " writeToWAL=" + writeToWAL +
+                      " reportLatency=" + reportLatency);
 
             PeInputSplit newSplit =
-              new PeInputSplit(startRow, rows, totalRows, sampleRate, clients,
+              new PeInputSplit(tableName, startRow, rows, totalRows, sampleRate, clients,
                 flushCommits, writeToWAL, reportLatency);
             splitList.add(newSplit);
           }
@@ -472,6 +488,7 @@ public class PerformanceEvaluation extends Configured implements Tool {
       };
 
       // Evaluation task
+      pe.tableName = value.getTableName();
       long elapsedTime = this.pe.runOneClient(this.cmd, value.getStartRow(),
         value.getRows(), value.getTotalRows(), value.getSampleRate(),
         value.isFlushCommits(), value.isWriteToWAL(), value.isReportLatency(),
@@ -672,7 +689,8 @@ public class PerformanceEvaluation extends Configured implements Tool {
     try {
       for (int i = 0; i < 10; i++) {
         for (int j = 0; j < N; j++) {
-          String s = "startRow=" + ((j * perClientRows) + (i * (perClientRows/10))) +
+          String s = "tableName=" + Bytes.toString(this.tableName) +
+          ", startRow=" + ((j * perClientRows) + (i * (perClientRows/10))) +
           ", perClientRunRows=" + (perClientRows / 10) +
           ", totalRows=" + this.R +
           ", sampleRate=" + this.sampleRate +
@@ -1384,6 +1402,12 @@ public class PerformanceEvaluation extends Configured implements Tool {
     }
 
     try {
+      // MR-NOTE: if you are adding a property that is used to control an operation
+      // like put(), get(), scan(), ... you must also add it as part of the MR
+      // input, take a look at writeInputFile().
+      // Then you must adapt the LINE_PATTERN input regex,
+      // and parse the argument, take a look at PEInputFormat.getSplits().
+
       for (int i = 0; i < args.length; i++) {
         String cmd = args[i];
         if (cmd.equals("-h") || cmd.startsWith("--h")) {
