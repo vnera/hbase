53a1211cb84040549bc80b20e8660eaea7f2fab7
Jonathan M Hsieh <jmhsieh@apache.org>
2014-09-04 05:41:21 -0700

    HBASE-11643 Read and write MOB in HBase (Jingcheng Du) Adds hfile.format.version = 3 to mob tests
    Reason: MOB Feature
    Author: Jingcheng Du
    Ref: CDH-19420

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFile.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFile.java
@@ -937,7 +937,7 @@ public class StoreFile {
       return this.writer.getPath();
     }
 
-    boolean hasGeneralBloom() {
+    public boolean hasGeneralBloom() {
       return this.generalBloomFilterWriter != null;
     }
 
