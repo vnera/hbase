051f32f46b36c75e7398d4ed7fe6548e2b99dd75
Enis Soztutar <enis@apache.org>
2014-11-12 15:50:10 -0800

    HBASE-12445 hbase is removing all remaining cells immediately after the cell marked with marker = KeyValue.Type.DeleteColumn via PUT (Hani Nadra)
    Reason: Bug
    Author: Hani Nadra
    Ref: CDH-23170
    
    Conflicts:
    	hbase-client/src/main/java/org/apache/hadoop/hbase/protobuf/ProtobufUtil.java

--- hbase-client/src/main/java/org/apache/hadoop/hbase/protobuf/ProtobufUtil.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/protobuf/ProtobufUtil.java
@@ -1078,6 +1078,7 @@ public final class ProtobufUtil {
       if (values != null && values.size() > 0) {
         for (Cell cell: values) {
           KeyValue kv = KeyValueUtil.ensureKeyValue(cell);
+          valueBuilder.clear();
           valueBuilder.setQualifier(ByteStringer.wrap(
               kv.getQualifierArray(), kv.getQualifierOffset(), kv.getQualifierLength()));
           valueBuilder.setValue(ByteStringer.wrap(
@@ -1140,6 +1141,7 @@ public final class ProtobufUtil {
       columnBuilder.setFamily(ByteStringer.wrap(family.getKey()));
       for (Cell cell: family.getValue()) {
         KeyValue kv = KeyValueUtil.ensureKeyValue(cell);
+        valueBuilder.clear();
         valueBuilder.setQualifier(ByteStringer.wrap(
             kv.getQualifierArray(), kv.getQualifierOffset(), kv.getQualifierLength()));
         valueBuilder.setValue(ByteStringer.wrap(
@@ -1965,7 +1967,7 @@ public final class ProtobufUtil {
           for (Permission.Action a : actions) {
             builder.addAction(toPermissionAction(a));
           }
-	}
+        }
         ret.setNamespacePermission(builder);
         return ret.build();
       } else if (tablePerm.hasTable()) {
