a86dc5ac64978d0c58741f68814f20a9cb7acd4b
Enis Soztutar <enis@apache.org>
2015-10-12 20:37:34 -0700

    HBASE-14501 NPE in replication with TDE
    Reason: Bug
    Author: Enis Soztutar
    Ref: CDH-32912
    
    Conflicts:
    	hbase-common/src/main/java/org/apache/hadoop/hbase/KeyValue.java
    	hbase-common/src/main/java/org/apache/hadoop/hbase/KeyValueUtil.java
    
    Conflicts:
    	hbase-common/src/main/java/org/apache/hadoop/hbase/KeyValueUtil.java
    	hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
    
    Change-Id: Ia9e3a44e46dcb8b98e643a54fbeacc0e433f9d42

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
@@ -653,6 +653,7 @@ public class ReplicationSource extends Thread
       if (!CellUtil.matchingRow(cells.get(i), lastCell)) {
         distinctRowKeys++;
       }
+      lastCell = cells.get(i);
     }
     return distinctRowKeys;
   }
