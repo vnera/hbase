ebe2be97e1584aad53d65e352615a8c9ad4d9f96
stack <stack@duboce.net>
2015-12-29 10:46:02 -0800

    HBASE-14205 RegionCoprocessorHost System.nanoTime() performance bottleneck Reason: Partner request; ngdata Author: Andrew Purtell Ref: CDH-35116
    Change-Id: I708e46097b2fdffaff350219327e5698a807fef3

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/MetricsRegionWrapperStub.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/MetricsRegionWrapperStub.java
@@ -18,11 +18,6 @@
 
 package org.apache.hadoop.hbase.regionserver;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
-
 public class MetricsRegionWrapperStub implements MetricsRegionWrapper {
 
   @Override
@@ -83,10 +78,5 @@ public class MetricsRegionWrapperStub implements MetricsRegionWrapper {
   @Override
   public long getNumCompactionsCompleted() {
     return 0;
-  }
-
-  @Override
-  public Map<String, DescriptiveStatistics> getCoprocessorExecutionStatistics() {
-    return new HashMap<String, DescriptiveStatistics>();
   }
 }
