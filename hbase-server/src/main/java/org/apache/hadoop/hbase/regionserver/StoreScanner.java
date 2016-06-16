8e7f74f85df192eef6b9191b94623444ffba2f74
Huaxiang Sun <hsun@cloudera.com>
2016-06-16 16:32:02 -0700

    HBASE-15496 Throw RowTooBigException only for user scan/get (Guanghao Zhang)
    Reason: Bug
    Author: Guanghao Zhang
    Ref: CDH-41134
    
    Change-Id: I54659b55983506bf57f84aa08dadaf0b5cb8b468

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreScanner.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreScanner.java
@@ -356,7 +356,7 @@ public class StoreScanner extends NonReversedNonLazyKeyValueScanner
       if (!isParallelSeek) {
         long totalScannersSoughtBytes = 0;
         for (KeyValueScanner scanner : scanners) {
-          if (totalScannersSoughtBytes >= maxRowSize) {
+          if (matcher.isUserScan() && totalScannersSoughtBytes >= maxRowSize) {
             throw new RowTooBigException("Max row size allowed: " + maxRowSize
               + ", but row is bigger than that");
           }
@@ -579,7 +579,7 @@ public class StoreScanner extends NonReversedNonLazyKeyValueScanner
             scannerContext.incrementSizeProgress(CellUtil.estimatedHeapSizeOfWithoutTags(cell));
             scannerContext.incrementBatchProgress(1);
 
-            if (totalBytesRead > maxRowSize) {
+            if (matcher.isUserScan() && totalBytesRead > maxRowSize) {
               throw new RowTooBigException("Max row size allowed: " + maxRowSize
               + ", but the row is bigger than that.");
             }
