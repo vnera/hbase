6caf28d130ead177402ab7105616cbfee0417c47
Sean Busbey <busbey@cloudera.com>
2016-08-05 14:35:31 -0500

    Revert "HBASE-16288 HFile intermediate block level indexes might recurse forever creating multi TB files"
    This reverts commit 0fa500dd8f4cd5b3ce434a4cd963c49f61449f86.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileWriterV2.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileWriterV2.java
@@ -130,8 +130,6 @@ public class HFileWriterV2 extends AbstractHFileWriter {
         cacheIndexesOnWrite ? name : null);
     dataBlockIndexWriter.setMaxChunkSize(
         HFileBlockIndex.getMaxChunkSize(conf));
-    dataBlockIndexWriter.setMinIndexNumEntries(
-        HFileBlockIndex.getMinIndexNumEntries(conf));
     inlineBlockWriters.add(dataBlockIndexWriter);
 
     // Meta data block index writer
