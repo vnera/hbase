7694d3780c8a2e6bc9d7c10fa3b81bf0f56e3cac
Duo Zhang <zhangduo@apache.org>
2019-08-05 16:19:05 +0800

    HBASE-22539 WAL corruption due to early DBBs re-use when Durability.ASYNC_WAL is used (#437)
    Signed-off-by: Zheng Hu <openinx@gmail.com>
    (cherry picked from commit fe2409971cfd1de3b7f55fdd8447b9dddf57796a)
    
    Change-Id: I95d20990a756eaa0c69ff9276a082daadfb3b4a6
    Author: Duo Zhang <zhangduo@apache.org>
    Reason: Bug
    Ref: CDH-81986

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestAsyncFSWAL.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestAsyncFSWAL.java
@@ -80,7 +80,7 @@ public class TestAsyncFSWAL extends AbstractTestFSWAL {
         suffix, GROUP, CHANNEL_CLASS) {
 
       @Override
-      void atHeadOfRingBufferEventHandlerAppend() {
+      protected void atHeadOfRingBufferEventHandlerAppend() {
         action.run();
         super.atHeadOfRingBufferEventHandlerAppend();
       }
